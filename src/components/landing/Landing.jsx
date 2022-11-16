624614e849d6d817f32ca65760eb5ec8792c5d1c
Alireza Nikzad <alireza.nikzdd@gmail.com>
2022-11-16 18:16:31 +0330

    install axios, map on data, show data and create loading

---
+++ src/components/landing/Landing.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+
+// API
+import { getCoin } from "../../services/api";
+
+// Components
+import Loader from "../loader/Loader";
+import Coin from "./Coin";
+
+const Landing = () => {
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const data = await getCoin();
+      console.log(data);
+      setCoins(data);
+    };
+    fetchAPI();
+  }, []);
+  return (
+    <>
+      <input type="text" placeholder="Search" />
+      {coins.length ? (
+        <div>
+          {coins.map((coin) => (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              image={coin.image}
+              symbol={coin.symbol}
+              price={coin.current_price}
+              marketCap={coin.market_cap}
+              priceChange={coin.market_cap_change_24h}
+            />
+          ))}
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </>
+  );
+};
+
+export default Landing;
